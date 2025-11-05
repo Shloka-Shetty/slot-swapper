79f0602a711734f3e8c253bfaf6e995d8ba5ee8c
Shloka-Shetty <shlokashetty536@gmail.com>
2025-11-05 13:08:05 +0530

    Initial SlotSwapper project

---
+++ backend/requirements.py
@@ -0,0 +1,9 @@
+# slot-swapper/backend/requirements.txt
+fastapi
+uvicorn[standard]
+sqlalchemy
+pydantic
+mysql-connector-python  # Pure Python MySQL driver
+python-dotenv           # To load .env file
+python-jose[cryptography]
+passlib[bcrypt]
